08b62de7b00225bcc50c36c2ee1086c650322dfc
mahimazes <54077294+mahimazes@users.noreply.github.com>
2022-10-15 11:47:06 -0600

    Time: 246 ms (27.40%), Space: 41.9 MB (30.73%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int[] RunningSum(int[] nums) {
+        
+        int[] res=new int[nums.Length];
+        int sum=0;
+        
+        for(int i=0;i<nums.Length;i++)
+        {
+            sum=sum+nums[i];
+            res[i]=sum;
+        }
+        
+        return res;
+    }
+}
