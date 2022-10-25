6948e73f84e25ef67b87e2a6c51fd34e766785e6
mahimazes <54077294+mahimazes@users.noreply.github.com>
2022-10-25 10:42:38 -0600

    Time: 55 ms (8.68%), Space: 25.3 MB (51.77%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.cs
@@ -0,0 +1,48 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      bool IsBadVersion(int version); */
+
+public class Solution : VersionControl {
+    public int FirstBadVersion(int n) {
+        
+        int left=1;
+        int right=n;
+        
+        while(left<right)
+        {
+            int mid=left+(right-left)/2;
+            
+            if(IsBadVersion(mid))
+            {
+                right=mid;
+            }
+            else
+            {
+                left=mid+1;
+            }
+        }
+        
+        return left;
+       // return RecursiveBadVersion(1,n);
+        
+    }
+    
+    public int RecursiveBadVersion(int start,int end)
+    {
+        
+        if(start==end)
+        {
+            return start;
+        }
+        
+        int mid=(start+(end-start))/2;
+        
+        if(!IsBadVersion(mid))
+        {
+            return RecursiveBadVersion(mid+1,end);
+        }
+        else
+        {
+            return RecursiveBadVersion(start,mid);
+        }
+    }
+}
