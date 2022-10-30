c91c9ce1f351b4f897f3a4ab605ddf13587bccaa
mahimazes <54077294+mahimazes@users.noreply.github.com>
2022-10-30 08:39:33 -0600

    Time: 34 ms (70.55%), Space: 25.4 MB (30.32%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cs
+++ 0070-climbing-stairs/0070-climbing-stairs.cs
@@ -1,31 +1,22 @@
 public class Solution {
     public int ClimbStairs(int n) {
         
-        int[] memo=new int[n+1];
-       return ClimbStairs(0,n,memo);
-       
-        
-    }
-    
-    public int ClimbStairs(int i,int n,int[] memo)
-    {
-        if(i==n)
+         if(n==1)
         {
             return 1;
         }
         
-        if(i>n)
-        {
-            return 0;
-        }
+        int[] dp =new int[n+1];
+        dp[1]=1;
+        dp[2]=2;
         
-        if(memo[i]>0)
-        {
-            return memo[i];
-        }
+        for(int i=3;i<=n;i++)
+            dp[i]=dp[i-1]+dp[i-2];
+       
+        return dp[n];
+       
         
-        memo[i]=ClimbStairs(i+1,n,memo)+ ClimbStairs(i+2,n,memo);
-        
-        return memo[i];
-    } 
+    }
+    
+
 }
