5b1be07f43ca6b75bfc59fdf1c922ecd9788b232
mahimazes <54077294+mahimazes@users.noreply.github.com>
2022-11-08 10:34:16 -0700

    Time: 505 ms (88.60%), Space: 0B (100.00%) - LeetHub

--- 0603-consecutive-available-seats/0603-consecutive-available-seats.sql
+++ 0603-consecutive-available-seats/0603-consecutive-available-seats.sql
@@ -1,6 +1,6 @@
 /* Write your T-SQL query statement below */
 
-select
+/*select
     seat_id
 from Cinema C1
 where  C1.free=1 and 
@@ -10,3 +10,9 @@ where  C1.free=1 and
        ((seat_id+1) in (select C2.seat_id 
                         from Cinema C2
                         where  C2.free=1)))
+                        
+                        */
+                        
+select distinct C1.seat_id
+from Cinema C1 join Cinema C2 on abs(C1.seat_id-C2.seat_id)=1
+where C1.free=1 and C2.free=1
