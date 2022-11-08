bd3c5b5591d46e732bd0d7e939234f85bd0c1e1a
mahimazes <54077294+mahimazes@users.noreply.github.com>
2022-11-08 10:04:52 -0700

    Time: 1660 ms (13.36%), Space: 0B (100.00%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -1,6 +1,6 @@
 /* Write your T-SQL query statement below */
 
-
+/*
 select top 1 ord.customer_number 
 from (
     select
@@ -9,3 +9,9 @@ from (
     group by customer_number
 ) as ord
 order by ord.ct desc
+*/
+
+select top 1 customer_number
+from Orders
+group by customer_number
+order by count(*) desc
