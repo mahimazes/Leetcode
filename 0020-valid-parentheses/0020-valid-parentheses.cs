cd1a685acef473e73b796754b932f636e8c499e1
mahimazes <54077294+mahimazes@users.noreply.github.com>
2022-11-12 14:05:47 -0700

    Time: 122 ms (55.34%), Space: 36.4 MB (85.07%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cs
@@ -0,0 +1,41 @@
+public class Solution {
+    public bool IsValid(string s) {
+       
+        Stack<char> stringStack=new Stack<char>();
+        
+        char temp;
+        
+        foreach(char c in s)
+        {
+            if(c==')' || c=='}' || c==']')
+            {
+                if(stringStack.Count>0)
+                {
+                   temp=stringStack.Peek();
+                
+                    if( (temp=='(' && c==')') || ( temp=='{' && c=='}') || ( temp=='[' && c==']'))
+                    {   
+                        stringStack.Pop();
+                    }
+                    else
+                    {
+                        stringStack.Push(c);
+                    }
+                }
+                else
+                {
+                    return false;    
+                }
+            }
+            else
+            {
+                stringStack.Push(c);
+            }
+        }
+        
+        if(stringStack.Count>0)
+            return false;
+        
+        return true;
+    }
+}
