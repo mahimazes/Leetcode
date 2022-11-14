a718341133a64e84e6379aa6137d4e6a78c86df0
mahimazes <54077294+mahimazes@users.noreply.github.com>
2022-11-14 07:30:07 -0700

    Time: 207 ms (36.60%), Space: 39.9 MB (69.85%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cs
+++ 0724-find-pivot-index/0724-find-pivot-index.cs
@@ -1,27 +1,10 @@
 public class Solution {
     public int PivotIndex(int[] nums) {
-      
+        
         int sumfront=0,sumback=nums[nums.Length-1];
         int ind=-1;
         int front=0, back=nums.Length-1;
-       /* while(front<back)
-        {
-            if(sumfront<sumback)
-            {
-                sumfront=sumfront+nums[front];
-                front++;
-            }
-            else if(sumfront>sumback)
-            {
-                back--;
-                sumback=sumback+nums[back];
-            }
-            else if(sumfront==sumback)
-            {
-                return front;
-            }
-        }
-        */
+      
         int sum = 0, leftsum = 0;
         for(int x=0;x<nums.Length;x++) 
         {
