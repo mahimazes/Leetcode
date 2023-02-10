579c5b9308368f1e93b01be17f3cbe6cc6a99770
mahimazes <54077294+mahimazes@users.noreply.github.com>
2023-02-09 20:19:21 -0700

    Time: 137 ms, Memory: 43.5 MB - LeetHub

---
+++ next-greater-element-i/next-greater-element-i.cs
@@ -0,0 +1,37 @@
+public class Solution {
+    public int[] NextGreaterElement(int[] nums1, int[] nums2) {
+        
+        Stack<int> stack=new Stack<int>();
+        
+        Dictionary<int,int> hMap=new Dictionary<int,int>();
+        
+        for(int i=0;i<nums2.Length;i++)
+        {
+            int count=0;
+            while(stack.Count!=0 && nums2[i]>stack.Peek())
+            {   
+                hMap.Add(stack.Pop(),nums2[i]);
+            }
+            stack.Push(nums2[i]);
+
+            while(stack.Count!=0 && nums2[i]<stack.Peek())
+            {
+                stack.Pop();
+            }
+            
+        }
+        
+        int[] result=new int[nums1.Length];
+        
+        for(int i=0;i<nums1.Length;i++)
+        {
+            if(hMap.ContainsKey(nums1[i]))
+                result[i]=hMap[nums1[i]];
+            else
+                result[i]=-1;
+        }
+        
+        return result;
+        
+    }
+}
