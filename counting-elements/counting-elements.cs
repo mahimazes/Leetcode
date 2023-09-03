de5490e1feef45f3bf156c60fb5df709c8c08c76
mahimazes <54077294+mahimazes@users.noreply.github.com>
2023-09-03 09:23:12 -0600

    Time: 63 ms, Memory: 38.7 MB - LeetHub

--- counting-elements/counting-elements.cs
+++ counting-elements/counting-elements.cs
@@ -1,20 +1,23 @@
 public class Solution {
     public int CountElements(int[] arr) {
         
-        HashSet<int> set=new HashSet<int>();
+        HashSet<int> eSet=new HashSet<int>();
         
         for(int i=0;i<arr.Length;i++)
         {
-            set.Add(arr[i]);
+            eSet.Add(arr[i]);
         }
         
-        int count=0;
+        int ans=0;
         for(int i=0;i<arr.Length;i++)
         {
-            if(set.Contains(arr[i]+1))
-                count++;
+            if(eSet.Contains(arr[i]+1))
+            {
+                ans++;
+            }
+            
         }
         
-        return count;
+        return ans;
     }
 }
