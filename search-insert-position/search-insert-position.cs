1aee6ceda4dd65d3097401640c537da1eef31688
mahimazes <54077294+mahimazes@users.noreply.github.com>
2023-10-17 23:06:34 -0600

    Time: 83 ms, Memory: 39.6 MB - LeetHub

--- search-insert-position/search-insert-position.cs
+++ search-insert-position/search-insert-position.cs
@@ -4,29 +4,26 @@ public class Solution {
         int left=0;
         int right=nums.Length-1;
         
-         while(left<=right)
+        while(left<=right)
         {
-             int mid=left+(right-left)/2;
-             Console.WriteLine(mid);
-        
-             if(nums[mid]==target)
-             {
-                 return mid;
-             }
-            else if(nums[mid]>target)
+            int mid=left+(right-left)/2;
+            
+            if(nums[mid]==target)
             {
-                right=mid-1;
+                return mid;
             }
-            else if(nums[mid]<=target)
+            
+            if(nums[mid]<target)
             {
                 left=mid+1;
+            }
+            else
+            {
+                right=mid-1;
             }
         }
         
         return left;
         
-        
     }
-    
-   
 }
